bf1a7eeb749489c51bb0b3cd0db1bdd42177142c
Alicia Poindexter <alcampbel@gmail.com>
2016-01-12 20:53:19 -0800

    initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var PORT = process.env.PORT || 3000;
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+
+var app = express();
+
+app.set('view engine', 'jade');
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded( {extended: true} ));
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+
+
+
+app.listen(PORT, function(){
+  console.log('Listening on port ', PORT);
+});
